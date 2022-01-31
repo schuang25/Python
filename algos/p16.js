6d0ecd8c00f1d68c9212dc53336b8a4302ec2b3f
schuang25 <stephen.chuang25@gmail.com>
2022-01-31 09:40:07 -0800

    w4d1 algos

---
+++ algos/p16.js
@@ -0,0 +1,58 @@
+/* 
+  Recursively sum an arr of ints
+*/
+
+const nums1 = [1, 2, 3];
+const expected1 = 6;
+
+/**
+ * Add params if needed for recursion
+ * Recursively sums the given array.
+ * - Time: O(?).
+ * - Space: O(?).
+ * @param {Array<number>} nums
+ * @returns {number} The sum of the given nums.
+ */
+function sumArr(nums, i=0) {
+    // edge case 
+
+    // base case -> what is going to end the recursion
+    if (i === nums.length){
+        return 0
+    }
+
+    // recursive call
+    return nums[i] + sumArr(nums, i + 1)
+}
+
+console.log(sumArr(nums1));
+
+
+// *******************************************************
+
+
+/* 
+Recursive Sigma
+Input: integer
+Output: sum of integers from 1 to Input integer
+*/
+
+const num1 = 5;
+const expected1 = 15;
+// Explanation: (1+2+3+4+5)
+
+const num2 = 2.5;
+const expected2 = 3;
+// Explanation: (1+2)
+
+const num3 = -1;
+const expected3 = 0;
+
+/**
+ * Recursively sum the given int and every previous positive int.
+ * - Time: O(?).
+ * - Space: O(?).
+ * @param {number} num
+ * @returns {number}
+ */
+function recursiveSigma(num) {}
